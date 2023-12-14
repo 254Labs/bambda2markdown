a5f95aef18f01796a355e4e6d58463ef228bd292
Moses <36628352+genuinemoses@users.noreply.github.com>
2023-12-15 00:29:16 +0530

    Create MetadataParserException.java

---
+++ src/org/254labs/bambda2markdown/MetadataParserException.java
@@ -0,0 +1,8 @@
+package org.labs254.bambda2markdown;
+
+public class MetadataParserException extends RuntimeException {
+
+    public MetadataParserException(String s) {
+        System.out.println(s);
+    }
+}
